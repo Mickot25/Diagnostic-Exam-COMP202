46eb717d9fed9ad8f34cb858026ff99fb0a8662c
Michael Contreras <michaelcontrerasr25@gmail.com>
2017-01-18 22:16:17 +0800

    start

---
+++ Diagnostic Exam COMP202/Source.cpp
@@ -0,0 +1,12 @@
+/*Diagnostic exam in Comp 202 last January 18 2017.*/
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	char time, type;
+	double minutes, tcharge, ncharge
+
+}
